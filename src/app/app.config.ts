0401ba42063748d9f07eef46f1577284212cc0a6
Juan Pablo Marin Isaza <marinjuanpabloisaza@gmail.com>
2025-09-09 17:31:35 -0500

    icons multilenguaje

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +14,13 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: './i18n/', 
+        suffix: '.json'
+      }),
+      fallbackLang: 'en'
+    })
   ]
 };
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,19 +3,25 @@ import { AuthService, UserData } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'navbarCom',
   standalone: true,
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
-  imports: [CommonModule, RouterModule, RouterOutlet],
+  imports: [CommonModule, RouterModule, RouterOutlet, MatIconModule ],
 })
 export class NavbarComponent implements OnDestroy {
   user = signal<UserData | null>(null);
   private sub: Subscription;
 
-  constructor(public authService: AuthService) {
+  constructor(
+    private translate: TranslateService,
+    public authService: AuthService
+  ) {
+    
     // Suscribirse al BehaviorSubject de AuthService
     this.sub = this.authService.currentUser$.subscribe(u => {
       this.user.set(u);
@@ -25,8 +31,14 @@ export class NavbarComponent implements OnDestroy {
 
   logout() {
     this.authService.logout();
-    // no hace falta setear user, ya lo hará la suscripción
   }
+
+  switchLang() {
+  const current = this.translate.currentLang;
+  const nextLang = current === 'es' ? 'en' : 'es';
+  console.log(`🌐 Cambiando idioma de ${current} a ${nextLang}`);
+  this.translate.use(nextLang);
+}
 
   ngOnDestroy() {
     this.sub.unsubscribe(); // limpieza de suscripción
