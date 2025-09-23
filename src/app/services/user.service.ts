488e6c76c957d4baa5149231c42d040e83547130
Juan Pablo Marin Isaza <marinjuanpabloisaza@gmail.com>
2025-09-23 18:26:15 -0500

    modulo de registro funcional y reordenar navar con role y nombre

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, NgZone } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+
+export interface UserData {
+  id: string;
+  userName: string;
+  accessToken: string;
+  refreshToken: string;
+}
+
+interface RegisterResponse {
+  success: boolean;
+  code?: string;
+  data?: any;
+}
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+private apiUrl = `${environment.apiUrl}/user`;
+
+
+
+  constructor(private http: HttpClient, private ngZone: NgZone) {
+   
+  }
+
+  register(userName: string, password: string, name: string, lastName: string): Observable<RegisterResponse> {
+    return this.http.post<RegisterResponse>(`${this.apiUrl}/create`, { userName, password, name, lastName})
+      .pipe(
+        tap(res => {
+          if (res.success && res.data) {
+          
+          
+          }
+        })
+      );
+  }
+
+
+}
