383b364c34e080a28fdb48cb429db409989ea2ef
Juan Pablo Marin Isaza <marinjuanpabloisaza@gmail.com>
2025-09-27 18:34:37 -0500

    services change colors

--- src/app/ui/pages/service-detail/service-detail.ts
+++ src/app/ui/pages/service-detail/service-detail.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,7 +8,36 @@ import { Router } from '@angular/router';
   styleUrl: './service-detail.css'
 })
 export class ServiceDetail {
-  constructor(private router: Router) {}
+    lastSegment = signal<string>('');
+
+     technologies = signal([
+    'React', 'Node.js', 'Python', 'Django', 'PostgreSQL',
+    'MongoDB', 'AWS', 'Docker', 'Kubernetes', 'Git'
+  ]);
+
+    steps = signal([
+    'Análisis de requerimientos',
+    'Diseño de arquitectura',
+    'Desarrollo iterativo',
+    'Testing y QA',
+    'Despliegue y monitoreo'
+  ]);
+
+
+   useCases = signal([
+    'Sistemas de gestión empresarial',
+    'Plataformas de e-commerce',
+    'Aplicaciones móviles',
+    'Portales web corporativos',
+    'APIs y microservicios'
+  ]);
+  constructor(private router: Router) {
+
+    const url = this.router.url; // ejemplo: /service/consultoria-tecnologica
+    this.lastSegment.set(url.split('/').pop() || '')
+  }
+
+  
 
   goBack() {
     this.router.navigate(['/dashboard']);
